438c722f7e3394c27ed56ec19bbad9577c604219
StandardGiraffe <dannyfekete@gmail.com>
2018-08-30 23:04:44 +0000

    Refactored if-chain to be tidier.

--- advanced-sort.js
+++ advanced-sort.js
@@ -12,7 +12,8 @@ var students = [
   { id: 1, name: "bruce",    age: 40 },
   { id: 2, name: "zoidberg", age: 22 },
   { id: 3, name: "alex",     age: 22 },
-  { id: 4, name: "alex",     age: 30 }
+  { id: 4, name: "alex",     age: 30 },
+  { id: 5, name: "alex",     age: 400 }
 ];
 
 var customSort = function (array) {
@@ -23,11 +24,7 @@ var customSort = function (array) {
     if (a.name > b.name) {
       return 1;
     } else if (a.name == b.name){
-      if (a.age > b.age) {
-        return -1;
-      } else if (a.age == b.age){
-        return 0;
-      } else return 1;
+      return (b.age - a.age);
     }
   })
 
